287806f8fbcf103ce3b05e10c2a3818806e34303
JSuupaSellers <joshua.sellersdq@gmail.com>
2017-01-11 18:56:26 -0500

    Added Comment on Menu Activity

--- app/src/main/java/com/jumpingbeanapps/android/custombeatbox/MainActivity.java
+++ app/src/main/java/com/jumpingbeanapps/android/custombeatbox/MainActivity.java
@@ -4,7 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-//Comment menu Branch
+//Comment menu
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
