19210b5725da1741c673995765531d54807800c8
Thomas <hartmannthomas98@gmail.com>
2017-01-12 22:52:05 +0100

    -Added core UI functionality and set up interface to transfer model data to the detail view. -Added test code (will be removed in next commit)

--- app/src/main/java/com/jumpingbeanapps/android/custombeatbox/MainActivity.java
+++ app/src/main/java/com/jumpingbeanapps/android/custombeatbox/MainActivity.java
@@ -1,13 +1,3 @@
 package com.jumpingbeanapps.android.custombeatbox;
 
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-
-public class MainActivity extends AppCompatActivity {
-//Comment menu
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
-}
+public class MainActivity extends SingleFragmentActivity {}
---
+++ app/src/main/java/com/jumpingbeanapps/android/custombeatbox/Sound.java
@@ -0,0 +1,18 @@
+package com.jumpingbeanapps.android.custombeatbox;
+
+class Sound {
+
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    void play(){
+
+    }
+}
